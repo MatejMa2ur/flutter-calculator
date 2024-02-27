6a79756912d02f4a96ff8df185379f6df15c8154
MatejMa2ur <matejmazur@outlook.com>
2024-02-27 10:50:04 +0100

    feat(test): Fix the height problem

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,218 +15,230 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: NeumorphicTheme.baseColor(context),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Neumorphic(
-            margin: const EdgeInsets.fromLTRB(24, 10, 24, 10),
-            style: const NeumorphicStyle(depth: -4),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          "[ ${calculator.stack.join(",")} ]",
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 20.0,
+      body: LayoutBuilder(
+        // added LayoutBuilder
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Neumorphic(
+                    margin: const EdgeInsets.fromLTRB(24, 10, 24, 10),
+                    style: const NeumorphicStyle(depth: -4),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Text(
+                                  "[ ${calculator.stack.join(",")} ]",
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                  ),
+                                  textAlign: TextAlign.right,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Text(
+                                  calculator.input,
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 32.0,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.right,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: StaggeredGrid.count(
+                      mainAxisSpacing: 5,
+                      crossAxisCount: 4,
+                      children: <Widget>[
+                        // 1st Row
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton('CLR', context,
+                              color: NeumorphicColors.accent,
+                              onClicked: calculator.clearInput),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton('POP', context,
+                              color: NeumorphicColors.accent,
+                              onClicked: calculator.popNumber),
+                        ),
+                        //buildButton('/', context),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton('/', context,
+                              bgColor: NeumorphicColors.accent,
+                              color: Colors.white,
+                              onClicked: calculator.division),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton('*', context,
+                              bgColor: NeumorphicColors.accent,
+                              color: Colors.white,
+                              onClicked: calculator.multiplication),
+                        ),
+
+                        // 2nd Row
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '7',
+                            context,
                           ),
-                          textAlign: TextAlign.right,
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text(
-                          calculator.input,
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 32.0,
-                              fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.right,
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '8',
+                            context,
+                          ),
                         ),
-                      ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '9',
+                            context,
+                          ),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton('-', context,
+                              bgColor: NeumorphicColors.accent,
+                              color: Colors.white,
+                              onClicked: calculator.subtraction),
+                        ),
+
+                        // 3rd Row
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '4',
+                            context,
+                          ),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '5',
+                            context,
+                          ),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '6',
+                            context,
+                          ),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton('+', context,
+                              bgColor: NeumorphicColors.accent,
+                              color: Colors.white,
+                              onClicked: calculator.addition),
+                        ),
+
+                        // 4th Row
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '1',
+                            context,
+                          ),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '2',
+                            context,
+                          ),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '3',
+                            context,
+                          ),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 2,
+                          child: buildButton('ENT', context,
+                              bgColor: NeumorphicColors.accent,
+                              color: Colors.white,
+                              onClicked: calculator.addNumber),
+                        ),
+
+                        // 5th Row
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton('+/-', context,
+                              onClicked: calculator.invertNumber),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton(
+                            '0',
+                            context,
+                          ),
+                        ),
+                        StaggeredGridTile.count(
+                          crossAxisCellCount: 1,
+                          mainAxisCellCount: 1,
+                          child: buildButton('.', context,
+                              onClicked: calculator.addFloating),
+                        ), // buildButton('=', context),
+                      ],
                     ),
-                  ],
-                ),
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: StaggeredGrid.count(
-              mainAxisSpacing: 5,
-              crossAxisCount: 4,
-              children: <Widget>[
-                // 1st Row
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton('CLR', context,
-                      color: NeumorphicColors.accent,
-                      onClicked: calculator.clearInput),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton('POP', context,
-                      color: NeumorphicColors.accent,
-                      onClicked: calculator.popNumber),
-                ),
-                //buildButton('/', context),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton('/', context,
-                      bgColor: NeumorphicColors.accent,
-                      color: Colors.white,
-                      onClicked: calculator.division),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton('*', context,
-                      bgColor: NeumorphicColors.accent,
-                      color: Colors.white,
-                      onClicked: calculator.multiplication),
-                ),
-
-                // 2nd Row
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '7',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '8',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '9',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton('-', context,
-                      bgColor: NeumorphicColors.accent,
-                      color: Colors.white,
-                      onClicked: calculator.subtraction),
-                ),
-
-                // 3rd Row
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '4',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '5',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '6',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton('+', context,
-                      bgColor: NeumorphicColors.accent,
-                      color: Colors.white,
-                      onClicked: calculator.addition),
-                ),
-
-                // 4th Row
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '1',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '2',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '3',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 2,
-                  child: buildButton('ENT', context,
-                      bgColor: NeumorphicColors.accent,
-                      color: Colors.white,
-                      onClicked: calculator.addNumber),
-                ),
-
-                // 5th Row
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton('+/-', context,
-                      onClicked: calculator.invertNumber),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton(
-                    '0',
-                    context,
-                  ),
-                ),
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 1,
-                  child: buildButton('.', context,
-                      onClicked: calculator.addFloating),
-                ), // buildButton('=', context),
-              ],
-            ),
-          )
-        ],
+          );
+        },
       ),
     );
   }
